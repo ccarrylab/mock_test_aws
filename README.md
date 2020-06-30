1c0e56e3ef060600598d3917089f6815b6c897ab
Cohen Carryl <cohen.carryl@gmail.com>
2020-06-30 11:05:34 +0100

    full project update

---
+++ README.md
@@ -0,0 +1,66 @@
+# Steps to run this:
+
+go to terminal and go to inside this folder
+
+### Setup Python Virtual Environment
+> pip install virtualenv
+
+> virtualenv myvirtualevn
+
+For Mac OS / Linux
+> source myvirtualevn/bin/activate
+
+For Windows
+> myvirtualevn\Scripts\activate
+
+
+### then below commands in sequence:
+pip install -r requirements.txt
+
+pytest test/test_my_handler.py
+
+### check coverage:
+pytest --cov=src tests/
+
+Note: There are couple of things to note when you run/test this code.
+
+- As 'describe_alarm_history' method was not implemented in actual moto library
+we implemented this in the code with the sample alarms provide.
+The sample alarms are given below:
+```python
+alarms = [{
+        "Timestamp": "2020-06-25T18:59:06.442Z",
+        "HistoryItemType": "StateUpdate",
+        "AlarmName": "myalarm",
+        "HistoryData": "{\"version\":\"1.0\",\"oldState\":{\"stateValue\":\"ALARM\",\"stateReason\":\"testing purposes\"},\"newState\":{\"stateValue\":\"OK\",\"stateReason\":\"Threshold Crossed: 2 datapoints were not greater than the threshold (70.0). The most recent datapoints: [38.958, 40.292].\",\"stateReasonData\":{\"version\":\"1.0\",\"queryDate\":\"2014-04-09T18:59:06.419+0000\",\"startDate\":\"2014-04-09T18:44:00.000+0000\",\"statistic\":\"Average\",\"period\":300,\"recentDatapoints\":[38.958,40.292],\"threshold\":70.0}}}",
+        "HistorySummary": "Alarm updated from ALARM to OK arn:aws:sns:us-east-1:123456789012:my-topic"
+    },
+    {
+        "Timestamp": "2020-06-25T19:59:06.442Z",
+        "HistoryItemType": "StateUpdate",
+        "AlarmName": "myalarm",
+        "HistoryData": "{\"version\":\"1.0\",\"oldState\":{\"stateValue\":\"OK\",\"stateReason\":\"Threshold Crossed: 2 datapoints were not greater than the threshold (70.0). The most recent datapoints: [38.839999999999996, 39.714].\",\"stateReasonData\":{\"version\":\"1.0\",\"queryDate\":\"2014-03-11T22:45:41.569+0000\",\"startDate\":\"2014-03-11T22:30:00.000+0000\",\"statistic\":\"Average\",\"period\":300,\"recentDatapoints\":[38.839999999999996,39.714],\"threshold\":70.0}},\"newState\":{\"stateValue\":\"ALARM\",\"stateReason\":\"testing purposes\"}}",
+        "HistorySummary": "Alarm updated from OK to ALARM arn:aws:sns:us-east-1:123456789012:my-topic"
+    },
+    {
+        "Timestamp": "2020-06-25T20:59:06.442Z",
+        "HistoryItemType": "StateUpdate",
+        "AlarmName": "myalarm",
+        "HistoryData": "{\"version\":\"1.0\",\"oldState\":{\"stateValue\":\"OK\",\"stateReason\":\"Threshold Crossed: 2 datapoints were not greater than the threshold (70.0). The most recent datapoints: [38.839999999999996, 39.714].\",\"stateReasonData\":{\"version\":\"1.0\",\"queryDate\":\"2014-03-11T22:45:41.569+0000\",\"startDate\":\"2014-03-11T22:30:00.000+0000\",\"statistic\":\"Average\",\"period\":300,\"recentDatapoints\":[38.839999999999996,39.714],\"threshold\":70.0}},\"newState\":{\"stateValue\":\"ALARM\",\"stateReason\":\"testing purposes\"}}",
+        "HistorySummary": "Alarm updated from OK to ALARM arn:aws:sns:us-east-1:123456789012:my-topic"
+    }]
+```
+
+This is just for testing purpose, so if you want to modify it you can go below
+and modify it according to your alarms and need to modify the test cases accordingly:
+> package -> moto -> cloudwatch -> responses.py -> line 142
+
+- In the testing we use environment variable SUBSCRIPTORS_TO_REPORT.
+If there is no data get found in that variable then by default it will take 'testlambda1@mailinator.com' as a value.
+And if any emails get sent to 'testlambda1@mailinator.com' then you can see those by hitting the below url:
+https://www.mailinator.com/v3/index.jsp?zone=public&query=testlambda1
+
+
+### Deactivate the virtual environment
+To decativate the virtual environment and use your original Python environment, simply type ‘deactivate’.
+> deactivate
---
+++ src/my_handler.py
@@ -0,0 +1,209 @@
+import boto3
+import json
+from json2html import *
+#from jsonconv import *
+import time
+from os import getenv
+from datetime import datetime, timedelta, tzinfo, timezone
+
+# SUBSCRIPTORS_TO_REPORT = getenv('SUBSCRIPTORS_TO_REPORT', 'hoopers-aws@abc.com')
+SUBSCRIPTORS_TO_REPORT = getenv('SUBSCRIPTORS_TO_REPORT', 'testlambda1@mailinator.com')
+
+def get_subs_endpoint(topic):
+    end_points = []
+    snsclient = boto3.client('sns', region_name="us-east-1")
+    paginator = snsclient.get_paginator('list_subscriptions_by_topic')
+    #print("Topic: ", topic)
+    try:
+        for response in paginator.paginate(TopicArn=topic):
+            #print("Response: ", response)
+            if 'Subscriptions' in response:
+                #print("Subscriptions: ", response['Subscriptions'])
+                for subscription in response['Subscriptions']:
+                    #print("EndPoint: ", subscription['Endpoint'])
+                    end_points.append(subscription['Endpoint'].lower())
+    except Exception as e:
+        print("Error getting topic: ", topic)
+        print(e)
+    # print ("End Points", end_points)
+    return  end_points
+
+
+
+def get_alarms(start_date, end_date = None, offset = None):
+    time_diff = None
+    if offset:
+        time_diff = timedelta(hours=offset)
+        print("Timediff=>", time_diff)
+    alarms = {}
+    cloudwatch = boto3.client('cloudwatch', region_name="us-east-1")
+
+    paginator = cloudwatch.get_paginator('describe_alarm_history')
+
+    # print("Alarms Date: ", start_date)
+
+    if end_date:
+        # print("Alarms from: ", end_date)
+        page_iterator = paginator.paginate(HistoryItemType='Action',
+                                            StartDate=start_date,
+                                            EndDate=end_date)
+    else:
+        page_iterator = paginator.paginate(HistoryItemType='Action',
+                                            StartDate=start_date)
+
+    #central = time.timezone('US/Central')
+
+    for response in page_iterator:
+        # print(response)
+
+        for alarm in response['AlarmHistoryItems']:
+
+            # print("AlarmName: [", alarm['AlarmName'], "]")
+
+            # print("HistorySummary: ", alarm['HistorySummary'])
+
+            alarm['Timestamp'] = alarm['Timestamp'].replace(tzinfo=None)
+
+            if time_diff and alarm['Timestamp']: # Adjust time
+
+                alarm['Timestamp'] = alarm['Timestamp'] + time_diff
+
+                print("Timestamp CT: ", alarm['Timestamp'])
+
+            else:
+
+                print("Timestamp UTC: ", alarm['Timestamp'])
+
+            add_alarm = True
+
+            if end_date and (alarm['Timestamp'] < start_date or alarm['Timestamp'] > end_date):
+
+                # print("Alarm outside time frame.... discarded.")
+
+                add_alarm = False
+
+            if add_alarm:
+
+                if alarm['AlarmName'] in alarms:
+
+                    #print("Alarms from Old: ", alarms)
+
+                    alarms[alarm['AlarmName']]['count'] += 1
+
+                    #print("Alarm Name Duplicated: ", alarm['AlarmName'])
+
+                else:
+
+                    #print("Alarms from New: ", alarms)
+
+                    alarms.update( { alarm['AlarmName'] : {'end_points' : get_subs_endpoint(alarm['HistorySummary'][alarm['HistorySummary'].find('arn'):]), 'count' : 1} } )
+
+                    #print("Alarm Name New: ", alarm['AlarmName'])
+
+
+
+    # print("Alarms: ", alarms)
+
+    return alarms
+
+
+
+def get_alarm_to_report(subscriptors, start_date, end_date = None, offset = None):
+
+    alarms_to_report = []
+
+    alarms = get_alarms(start_date, end_date, offset)
+    print("Alarms=>: ", alarms)
+    print("Subscriptors=>", subscriptors)
+
+    for alarm in sorted(alarms):
+
+        # print("EndPoint=>: ", alarms[alarm]['end_points'])
+
+        if (alarms[alarm]['end_points']) and (subscriptors.issubset(alarms[alarm]['end_points']) or set(alarms[alarm]['end_points']).issubset(subscriptors)):
+
+            # print("Reporting: ", alarm)
+
+            alarms_to_report.append( {"Alarm": alarm, "# Events" : alarms[alarm]['count'], "Description" : "Test Description", "Status / JIRA Ticket" : "Test Jira Ticket 123" } )
+
+
+
+
+    # print("Alarms to report: ", alarms_to_report)
+
+    return alarms_to_report
+
+
+
+
+
+def lambda_handler(event, context):
+
+    time_offset = -6 # None
+    # time_offset = None
+
+    alarms_html = '<table border="1"><tr><th>Alarm</th><th># Events</th><th>Description</th><th>Status / JIRA Ticket</th></tr></table>'
+
+    dt = datetime.now() - timedelta(1) # yesterday
+
+    date = dt.replace(hour=0, minute=0, second=0, microsecond=0)
+
+    date_start = date_end = None
+
+    if event and 'date' in event:
+
+        date = event['date']
+
+        print("Date: ", date)
+
+    if event and 'date_start' in event:
+
+        date_start = datetime.strptime(event['date_start'], '%Y-%m-%d %H:%M:%S')
+
+        print("Date Ini: ", date_start)
+
+    if event and 'date_end' in event:
+
+        date_end = datetime.strptime(event['date_end'], '%Y-%m-%d %H:%M:%S.%f')
+
+        print("Date End: ", date_end)
+
+    alarms_to_report = {}
+
+    # TODO implement
+
+
+
+    if (date_start is not None) and (date_end is not None):
+
+        alarms_to_report = get_alarm_to_report(set(SUBSCRIPTORS_TO_REPORT.replace(" ", "").lower().split(",")), date_start, date_end, time_offset)
+
+    else:
+
+        alarms_to_report = get_alarm_to_report(set(SUBSCRIPTORS_TO_REPORT.replace(" ", "").lower().split(",")), date)
+
+    alarms_html = alarms_to_report
+
+    if event and 'format' in event:
+
+        if (event['format'] == 'html') and (alarms_to_report):
+
+            alarms_html = json2html.convert(json = alarms_to_report)
+
+
+
+
+
+    print("Alarms to report: ", alarms_to_report)
+
+    print(alarms_html)
+
+
+
+    return {
+
+        'statusCode': 200,
+
+        'body': alarms_html
+
+    }
